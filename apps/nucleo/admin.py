bb11c5f609f0c7a176b9134f3e9e19db4a9dbbf5
Cecilia Serrano <serranomartincecilia15@gmail.com>
2026-05-20 10:19:33 +0200

    ultimos cambios realizados

--- apps/nucleo/admin.py
+++ apps/nucleo/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Tenant
 
 
-@admin.register(Tenant)
-class TenantAdmin(admin.ModelAdmin):
-    list_display = ("nombre", "slug", "activo", "creado_en")
