2ad3dc0677c8e63c23c17f881574af819d040f6a
Cecilia Serrano <serranomartincecilia15@gmail.com>
2026-05-25 12:14:12 +0200

    correciones hechas

--- apps/nucleo/admin.py
+++ apps/nucleo/admin.py
@@ -1,16 +1,4 @@
 from django.contrib import admin
-
-from .models import Negocio
-
-@admin.register(Negocio)
-class NegocioAdmin(admin.ModelAdmin):
-    list_display = (
-        "nombre",
-        "identificador_fiscal",
-        "pais",
-        "actualizado_en",
-    )
-
 from .models import Perfil
 @admin.register(Perfil)
 class PerfilAdmin(admin.ModelAdmin):
