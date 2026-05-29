6b6eecadd22ca3021b51be3b8b45539f7473b31c
Manuel <manuelapariciodoeste@gmail.com>
2026-05-29 11:06:13 +0200

    Añado revisar_extraccion, detección de duplicados y exportacion de Excel 3 hojas

---
+++ apps/facturas/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Factura, LineaFactura, Proveedor, CategoriaGasto
+
+
+class LineaFacturaInline(admin.TabularInline):
+    model = LineaFactura
+    extra = 1
+
+
+@admin.register(Factura)
+class FacturaAdmin(admin.ModelAdmin):
+    list_display = ("numero_factura", "fecha_emision", "proveedor", "total")
+    search_fields = ("numero_factura",)
+    list_filter = ("fecha_emision", "proveedor", "categoria")
+    inlines = [LineaFacturaInline]
+
+
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "nif", "pais")
+    search_fields = ("nombre", "nif")
+
+
+@admin.register(CategoriaGasto)
+class CategoriaGastoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "deducible_iva", "cuenta_contable")
